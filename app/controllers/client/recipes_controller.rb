fde7ab16224d936924fe266e7805ebf9b3b3e750
mellowjoeys <mellowjoey@gmail.com>
2019-09-26 12:31:28 -0500

    added some bootstrap

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -9,10 +9,53 @@ class Client::RecipesController < ApplicationController
     render 'new.html.erb'
   end
 
+  def create
+    response = HTTP.post(
+                          "http://localhost:3000/api/recipes", 
+                          form: {
+                                  title: params[:title],
+                                  chef: params[:chef],
+                                  ingredients: params[:ingredients],
+                                  directions: params[:directions],
+                                  prep_time: params[:prep_time],
+                                  image_url: params[:image_url]
+                                }
+                        )
+    @recipe = response.parse
+
+    redirect_to "/client/recipes/#{@recipe["id"]}"
+  end
+
   def show
     response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
     @recipe = response.parse
     render 'show.html.erb'
   end
 
+  def edit
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    response = HTTP.patch(
+                          "http://localhost:3000/api/recipes/#{params[:id]}",
+                          form: {
+                                  title: params[:title],
+                                  chef: params[:chef],
+                                  ingredients: params[:ingredients],
+                                  directions: params[:directions],
+                                  prep_time: params[:prep_time],
+                                  image_url: params[:image_url]
+                                }
+                          )
+    @recipe = response.parse
+    redirect_to "/client/recipes/#{@recipe['id']}"
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/recipes/#{params[:id]}")
+    redirect_to "/client/recipes"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :client do
     get '/recipes' => 'recipes#index'
     get '/recipes/new' => 'recipes#new'
+    post '/recipes' => 'recipes#create'
     get '/recipes/:id' => 'recipes#show'
+    get '/recipes/:id/edit' => 'recipes#edit'
+    patch '/recipes/:id' => 'recipes#update'
+    delete '/recipes/:id' => 'recipes#destroy'
   end
 end
